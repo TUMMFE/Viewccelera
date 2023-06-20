8239ca2c41422c88a8728deb26e1256515b83c79
Jonathan Rapp <jonathan.rapp@tum.de>
2023-06-20 10:14:37 +0200

    Update analyser.h Added function filterAllData and corresponding signal for later implementation of progressbar while filtering filtering is now a moving average. This takes a lot of time

--- analyser.h
+++ analyser.h
@@ -14,19 +14,23 @@ public:
 
 public slots:
     void startDataProcessing(DataFile *dataFile);
+    void filterAllData(DataFile *dataFile);
     void startAnalysis(AnalysisDataFrame dataFrame);
     void saveEventData(AnalysisDataFrame dataFrame, QString filename);
     void saveAnalysedData(QList<AnalysisDataFrame> dataFrames, QString filename);
-    static bool compare(const AnalysisDataFrame& i, const AnalysisDataFrame& j);
 
 protected:
     QVector<double> integrate(QVector<double> time, QVector<double> data);
     QVector<double> integrateTrapezoidal(QVector<double> time, QVector<double> data);
     QVector<double> getAbs(QVector<double> x, QVector<double> y, QVector<double> z);
     QVector<double> movingAverage(QVector<double> data, int order);
+    static bool compare(const AnalysisDataFrame& i, const AnalysisDataFrame& j);
+
 
 signals:
     void analysisFinishied(AnalysisDataFrame dataFrame);
+    void progressFilterData(int progress);
 };
 
 #endif // ANALYSER_H
+
